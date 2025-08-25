6c64f26b636764bd5c1c7e8e80b8936fbb0b66c9
crist-17 <pci1234good@gmail.com>
2025-08-25 15:25:52 +0900

    add

---
+++ day01_0825/Ex_proj/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+import { ref } from 'vue'
+import Ex1 from './components/Ex1.vue';
+import Ex2 from './components/Ex2.vue';
+import Ex3 from './components/Ex3.vue';
+import Ex4 from './components/Ex4.vue';
+
+const pages = { Ex1, Ex2, Ex3, Ex4 }
+const activeComp = ref("Ex4")
+
+</script>
+
+<template>
+  <!-- <Ex1 />
+  <Ex2 />
+  <Ex3 />
+  <Ex4 /> -->
+  <button @click="activeComp = 'Ex1'">bind_바인드</button>
+  <button @click="activeComp = 'Ex2'">if_이프</button>
+  <button @click="activeComp = 'Ex3'">show_쇼우</button>
+  <button @click="activeComp = 'Ex4'">for_포</button>
+
+  <div id="comp_view">
+
+    <keep-alive>
+      <component :is="pages[activeComp]"></component>
+    </keep-alive>
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#comp_view {
+  margin-top: 100px;
+  border: 2px solid green;
+  border-radius: 25px;
+  padding: 50px;
+}
+</style>
---
+++ day01_0825/Ex_proj/src/components/Ex4.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>for문 배열</h1>
+    <ol>
+        <li v-for="food in manyFoods" :key="food">{{food}}</li>
+    </ol>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const manyFoods = ref([
+    'Burrito',
+    'Salad',
+    'Cake',
+    'Soup',
+    'Fish',
+    'Pizza',
+    'Rice',
+])
+</script>
+
+<style lang="scss" scoped></style>
